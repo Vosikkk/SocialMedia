a2d89f0e4c61b05e3c64a73e1c4bb250112dfb97
Vosik <vosikvos@gmail.com>
2024-02-06 14:56:00 +0200

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Саша Восколович on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
